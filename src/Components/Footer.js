aac4567950e95c0602c9743bf472288fa37c9d69
Mocy <zg.zaalishvili@gmail.com>
2022-04-20 20:21:03 +0400

    first commit

---
+++ src/Components/Footer.js
@@ -0,0 +1,23 @@
+const Footer = () => {
+  return (
+    <footer className="footerich">
+
+        <p>
+          Kutaisi International University is a public <br></br>
+          research university in Kutaisi, Georgia. Its <br></br>
+          construction was announce 12 September 2016. <br></br>
+          @2022 All Copyright Reserved
+        </p>
+
+        <ul>
+          <li><a href='#'>About Us</a></li>
+          <li><a href='#'>Contact Me</a></li>
+          <li><a href='#'>Privacy Policy</a></li>
+        </ul>
+
+        <img src='https://www.kiu.edu.ge/images/logo.svg' alt='logo' width={200} height={100}/>
+    </footer>
+  )
+}
+
+export default Footer
